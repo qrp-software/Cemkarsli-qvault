802f1bf3c5d6ea71875f89fc977e03ef20aaed6f
Cem Karslı <cem.karsli@qrp.com.tr>
2025-05-30 14:51:53 +0300

    updt

--- users/urls.py
+++ users/urls.py
@@ -7,28 +7,4 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("profile/", views.EditUserInfoView.as_view(), name="profile"),
-    path("password-reset/", views.PasswordResetView.as_view(), name="password_reset"),
-    path(
-        "password-reset/done/",
-        views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "password-reset/complete/",
-        views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    path(
-        "password-reset/<str:uidb64>/<str:token>/",
-        views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "password-change/", views.PasswordChangeView.as_view(), name="password_change"
-    ),
-    path(
-        "password-change/done/",
-        views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
 ]
