f920440f958da99ee68f135a8dd765a97708e1b4
Cem Karslı <cem.karsli@qrp.com.tr>
2025-06-27 15:22:12 +0300

    update

--- qvaultapp/settings.py
+++ qvaultapp/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
