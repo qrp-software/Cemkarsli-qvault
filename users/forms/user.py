03027f6d8cddb72c2a6d8dc585500b51613a2f64
Cem Karslı <cem.karsli@qrp.com.tr>
2025-06-30 16:59:39 +0300

    update-3

--- users/forms/user.py
+++ users/forms/user.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
 from users.models import User
 
 User = get_user_model()
@@ -10,3 +11,125 @@ class EditUserForm(forms.ModelForm):
     class Meta:
         fields = ("first_name", "last_name", "email")
         model = User
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Adınız'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Soyadınız'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'E-posta adresiniz'
+            }),
+        }
+        labels = {
+            'first_name': _('Ad'),
+            'last_name': _('Soyad'),
+            'email': _('E-posta'),
+        }
+
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Ad field'ı
+        self.fields['first_name'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Adınızı girin',
+            'autocomplete': 'given-name'
+        })
+        
+        # Soyad field'ı
+        self.fields['last_name'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Soyadınızı girin',
+            'autocomplete': 'family-name'
+        })
+        
+        # Kullanıcı adı field'ı
+        self.fields['username'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Kullanıcı adını girin',
+            'autocomplete': 'username'
+        })
+        
+        # E-posta field'ı
+        self.fields['email'].widget = forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'E-posta adresinizi girin',
+            'autocomplete': 'email'
+        })
+        
+        # Şifre field'ları
+        self.fields['password1'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Güçlü bir şifre oluşturun',
+            'autocomplete': 'new-password'
+        })
+        
+        self.fields['password2'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Şifrenizi tekrar girin',
+            'autocomplete': 'new-password'
+        })
+        
+        # Field labels
+        self.fields['first_name'].label = _('Ad')
+        self.fields['last_name'].label = _('Soyad')
+        self.fields['username'].label = _('Kullanıcı Adı')
+        self.fields['email'].label = _('E-posta')
+        self.fields['password1'].label = _('Şifre')
+        self.fields['password2'].label = _('Şifre (Tekrar)')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+        return user
+
+
+class EditUserPermissionsForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("is_active", "is_staff", "is_superuser", "can_share_systems")
+        widgets = {
+            'is_active': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'is_staff': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'is_superuser': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'can_share_systems': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+        }
+        labels = {
+            'is_active': _('Aktif'),
+            'is_staff': _('Staff Yetkisi'),
+            'is_superuser': _('Süper Kullanıcı'),
+            'can_share_systems': _('Sistem Paylaşım Yetkisi'),
+        }
+        help_texts = {
+            'is_active': _('Kullanıcının giriş yapabilmesini sağlar'),
+            'is_staff': _('Admin paneline erişim sağlar'),
+            'is_superuser': _('Tüm yönetici yetkilerini verir'),
+            'can_share_systems': _('Sistem paylaşımı yapabilmesine izin verir'),
+        }
--- users/views.py
+++ users/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 import logging
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views import View
 from django.contrib.auth.views import (
     LoginView as BaseLoginView,
     LogoutView as BaseLogoutView,
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 from users import forms
 
+User = get_user_model()
 logger = logging.getLogger(__name__)
 
 
@@ -22,7 +26,26 @@ class LogoutView(BaseLogoutView):
     pass
 
 
+class ProfileView(LoginRequiredMixin, View):
+    """Kullanıcı profil sayfası - herkes erişebilir"""
+    template_name = "users/profile.html"
+    form_class = forms.EditUserForm
+
+    def get(self, request):
+        form = self.form_class(instance=request.user)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            messages.success(request, _("Profil bilgileriniz başarıyla güncellendi."))
+            return redirect(reverse("users:profile"))
+        return render(request, self.template_name, {"form": form})
+
+
 class EditUserInfoView(LoginRequiredMixin, View):
+    """Eski view - geriye dönük uyumluluk için"""
     template_name = "users/edit_user_info.html"
     form_class = forms.EditUserForm
 
@@ -36,3 +59,95 @@ class EditUserInfoView(LoginRequiredMixin, View):
             form.save(commit=True)
             return redirect(reverse("home"))
         return render(request, self.template_name, {"form": form})
+
+
+class SuperUserRequiredMixin(UserPassesTestMixin):
+    """Sadece superuser'ların erişebileceği view'lar için mixin"""
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class AdminSettingsView(LoginRequiredMixin, SuperUserRequiredMixin, View):
+    """Yönetici ayarları ana sayfası - sadece superuser"""
+    template_name = "users/admin_settings.html"
+
+    def get(self, request):
+        users_count = User.objects.count()
+        recent_users = User.objects.order_by('-date_joined')[:5]
+        return render(request, self.template_name, {
+            'users_count': users_count,
+            'recent_users': recent_users,
+        })
+
+
+class UserManagementView(LoginRequiredMixin, SuperUserRequiredMixin, View):
+    """Kullanıcı yönetimi - kullanıcı listesi ve arama"""
+    template_name = "users/user_management.html"
+
+    def get(self, request):
+        search_query = request.GET.get('search', '')
+        users = User.objects.all()
+        
+        if search_query:
+            users = users.filter(
+                username__icontains=search_query
+            ) | users.filter(
+                first_name__icontains=search_query
+            ) | users.filter(
+                last_name__icontains=search_query
+            ) | users.filter(
+                email__icontains=search_query
+            )
+        
+        users = users.order_by('-date_joined')
+        
+        return render(request, self.template_name, {
+            'users': users,
+            'search_query': search_query,
+        })
+
+
+class CreateUserView(LoginRequiredMixin, SuperUserRequiredMixin, View):
+    """Yeni kullanıcı oluşturma"""
+    template_name = "users/create_user.html"
+    form_class = forms.CreateUserForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, _(f"Kullanıcı '{user.username}' başarıyla oluşturuldu."))
+            return redirect(reverse("users:user_management"))
+        return render(request, self.template_name, {"form": form})
+
+
+class EditUserPermissionsView(LoginRequiredMixin, SuperUserRequiredMixin, View):
+    """Kullanıcı yetkilerini düzenleme"""
+    template_name = "users/edit_user_permissions.html"
+    form_class = forms.EditUserPermissionsForm
+
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        if user == request.user:
+            messages.warning(request, _("Kendi yetkilendirinizi düzenleyemezsiniz."))
+            return redirect(reverse("users:user_management"))
+        
+        form = self.form_class(instance=user)
+        return render(request, self.template_name, {"form": form, "user": user})
+
+    def post(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        if user == request.user:
+            messages.warning(request, _("Kendi yetkilendirinizi düzenleyemezsiniz."))
+            return redirect(reverse("users:user_management"))
+        
+        form = self.form_class(instance=user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, _(f"'{user.username}' kullanıcısının yetkileri güncellendi."))
+            return redirect(reverse("users:user_management"))
+        return render(request, self.template_name, {"form": form, "user": user})
