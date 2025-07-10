175e19c9c6b510a6bdfedfe25615feb4187c3075
Cem Karslı <cem.karsli@qrp.com.tr>
2025-07-10 15:17:24 +0300

    faaliyetler

---
+++ keychain/migrations/0014_fix_uploaded_by_column.py
@@ -0,0 +1,28 @@
+# Generated manually to fix uploaded_by_id column
+from django.db import migrations
+
+
+def add_uploaded_by_column(apps, schema_editor):
+    """uploaded_by_id kolonunu ekle"""
+    from django.db import connection
+    
+    cursor = connection.cursor()
+    
+    # Mevcut kolonları kontrol et
+    cursor.execute("PRAGMA table_info(keychain_activityattachment)")
+    existing_columns = [row[1] for row in cursor.fetchall()]
+    
+    # uploaded_by_id kolonu yoksa ekle
+    if 'uploaded_by_id' not in existing_columns:
+        cursor.execute("ALTER TABLE keychain_activityattachment ADD COLUMN uploaded_by_id INTEGER REFERENCES auth_user(id)")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('keychain', '0012_remove_activity_attachment_activityattachment'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_uploaded_by_column),
+    ] 
